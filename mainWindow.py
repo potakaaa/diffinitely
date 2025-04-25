93400ad5376304baeb9abe6f5a4eab8a58d15f79
potakaaa <helbirog@gmail.com>
2025-04-25 19:22:52 +0800

    feat(ui): updated ui style

--- mainWindow.py
+++ mainWindow.py
@@ -15,7 +15,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setPalette(QPalette())
 
         # Initial font setup
-        self.initial_font_size = 10  # default font size
+        self.initial_font_size = 11  # default font size
         self.zoom_factor = 5  # zoom rate
         self.current_font_size = self.initial_font_size
 
