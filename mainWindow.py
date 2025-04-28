f1244ff9653754e9bcef04cf573cba6dcc571334
hdmGOAT <hdmGOAT@users.noreply.github.com>
2025-04-28 16:58:27 +0800

    bruh

---
+++ mainWindow.py
@@ -0,0 +1,38 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QIntValidator, QPalette
+from ui.main_window import Ui_MainWindow
+from handler.calc_buttons.calc_buttons_handlers import CalcButtonHandler
+from handler.menu_buttons.menu_buttons_handlers import ViewButtonsHandler, FileButtonsHandler, EditButtonsHandler
+from graphs.graph_manager import GraphManager
+from widgets.definite_input import DefiniteIntegralWidget
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.setWindowTitle("Diffinitely")
+
+        self.setPalette(QPalette())
+
+        self.graph_manager = GraphManager(self.graph_widget)
+
+        # Initial font setup
+        self.initial_font_size = 11  # default font size
+        self.zoom_factor = 5  # zoom rate
+        self.current_font_size = self.initial_font_size
+
+        # Menu Bar Buttons
+        self.view_buttons_handler = ViewButtonsHandler(self)
+        self.file_buttons_handler = FileButtonsHandler(self)
+        self.edit_buttons_handler = EditButtonsHandler(self)
+    
+
+        # N-value validator
+        self.n_value_edit.setValidator(QIntValidator(0, 1000))
+        self.calc_button_handler = CalcButtonHandler(self)
+
+
+
+
